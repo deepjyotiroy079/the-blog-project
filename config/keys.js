f52edbff1e9e35c2d7fadcb2ebbc2ad7fb80e33d
Deepjyoti Roy <deepjyotiroy079@gmail.com>
2020-08-10 13:24:06 +0530

    Added Dockerfile

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     // get you mongodb url here
-    MongoURI: 'mongodb+srv://<username>:<password>@cluster0-ax5zr.mongodb.net/test?retryWrites=true&w=majority'
+    // MongoURI: 'mongodb+srv://<username>:<password>@cluster0-ax5zr.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: 'mongodb://mongo:27017/blogs'
 }
